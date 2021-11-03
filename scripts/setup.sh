d5f76162875b1b5e2b8c0752f721892b81e4adec
Yordan Miladinov <jordanmiladinov@gmail.com>
2021-11-03 18:58:04 +0200

    new setup.sh concept

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ROOT=$(readlink -f $(dirname $BASH_SOURCE)/..)
-
 if [ -z "$1" ] ; then
     echo "usage: $0 <project-name>"
     exit 1
 fi
 
 # Replace __NAME__ with given arg.
-sed -i s/__NAME__/$1/g $ROOT/package.json
+sed -i s/__NAME__/$1/g ContractTemplate/package.json
 
-touch $ROOT/.secret
+touch ContractTemplate/.secret
+
+mv ContractTemplate "$1"
